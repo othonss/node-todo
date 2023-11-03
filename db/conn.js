b8e059bad4f33196acf568a512ad7a0b3d898a27
othonss <othon.santos.silva@gmail.com>
2023-11-03 20:46:28 -0300

    Atualização da conexão

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectModule: 'mysql2'
+    dialectModule: require('mysql2'),
 })
 
 try {
