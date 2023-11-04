583b98307ce0629fdc82548dcaa22fa34b1b4858
othonss <othon.santos.silva@gmail.com>
2023-11-04 10:55:03 -0300

    teste 26

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -20,7 +20,7 @@ module.exports = class TaskController {
     //Função responsável por mostrar as tarefas na tela principal (CRUD = R)
     static async showTasks(req, res){
         const tasks = await Tasks.findAll({raw: true})
-        res.render('/tasks', {tasks})
+        res.render('tasks/all', {tasks})
     }
 
     //Funções responsáveis por atualizar informações e status das tarefas  (CRUD = U)
