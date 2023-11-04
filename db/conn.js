1292cb781f32e0702ebbd2b332cb12a91c3d3086
othonss <othon.santos.silva@gmail.com>
2023-11-03 21:51:40 -0300

    Teste 5

--- db/conn.js
+++ db/conn.js
@@ -3,6 +3,7 @@ const {Sequelize} = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    port: 3306,
     dialectModule: require('mysql2'),
 })
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
-const PORT = 3006
+const PORT = 3001
 const app = express()
 
 const conn = require('./db/conn')
